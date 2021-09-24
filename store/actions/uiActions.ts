66c790a53e176f63c6ecd300c04726939908685e
BisiDavi <oludavidconnect@gmail.com>
2021-09-24 08:33:20 +0100

    redux constant updated

--- store/actions/uiActions.ts
+++ store/actions/uiActions.ts
@@ -1,9 +1,9 @@
-import { TOGGLE_MENU } from "@store/constants";
+import { TOGGLE_SIDEBAR_MENU } from "@store/constants";
 import { UIType } from "../../types/";
 
 export const UIActions =
     () => (dispatch: (arg0: { type: UIType }) => boolean) => {
         return dispatch({
-            type: TOGGLE_MENU,
+            type: TOGGLE_SIDEBAR_MENU,
         });
     };
