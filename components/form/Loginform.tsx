d8b08d79eb10b3210cb310a40d1314838b4d5312
BisiDavi <oludavidconnect@gmail.com>
2021-10-04 21:52:07 +0100

    auth updated

--- components/form/Loginform.tsx
+++ components/form/Loginform.tsx
@@ -3,16 +3,16 @@ import { IconButton, Tooltip } from "@material-ui/core";
 
 import AuthCard from "@components/AuthCard";
 import { authFormStyles } from "@styles/Authform.style";
-import { login, icons } from "@json/auth.json";
+import authJson from "@json/auth.json";
 import displayIcons from "@utils/displayIcons";
 
 export default function Loginform() {
     const classes = authFormStyles();
 
     return (
-        <AuthCard content={login}>
+        <AuthCard content={authJson.login}>
             <div className={classes.iconGroup}>
-                {icons.map((icon) => (
+                {authJson.icons.map((icon) => (
                     <Tooltip key={icon.name} title={icon.title}>
                         <IconButton onClick={() => signIn(icon.name)}>
                             {displayIcons(icon.name)}
