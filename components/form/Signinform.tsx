8e7c09398d57dd2ca358ce15bd1932a33213c002
BisiDavi <oludavidconnect@gmail.com>
2021-10-07 03:01:22 +0100

    auth updated

--- components/form/Signinform.tsx
+++ components/form/Signinform.tsx
@@ -1,5 +1,6 @@
 import { signIn } from "next-auth/client";
 import { IconButton, Tooltip } from "@material-ui/core";
+import { useRouter } from "next/router";
 
 import AuthCard from "@components/AuthCard";
 import { authFormStyles } from "@styles/Authform.style";
@@ -13,13 +14,21 @@ type providerType = {
 
 export default function Signinform({ providers }) {
     const classes = authFormStyles();
+    const router = useRouter();
+    const callbackURL: string = router.query.callbackUrl;
 
     return (
         <AuthCard content={authJson.login}>
             <div className={classes.iconGroup}>
                 {Object.values(providers).map((provider: providerType) => (
                     <Tooltip key={provider.name} title={provider.name}>
-                        <IconButton onClick={() => signIn(provider.id)}>
+                        <IconButton
+                            onClick={() =>
+                                signIn(provider.id, {
+                                    callbackUrl: callbackURL,
+                                })
+                            }
+                        >
                             {displayIcons(provider.id)}
                         </IconButton>
                     </Tooltip>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,34 +1,38 @@
 import NextAuth from "next-auth";
-import { useRouter } from "next/router";
 import Providers from "next-auth/providers";
 
+const options = {
+    providers: [
+        Providers.Google({
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+        }),
+        Providers.GitHub({
+            clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
+        }),
+    ],
+    pages: {
+        signIn: "/auth/signin",
+    },
+    database: process.env.DATABASE_URL,
+    secret: process.env.NEXT_PUBLIC_SECRET,
+    session: {
+        jwt: true,
+        maxAge: 30 * 24 * 60 * 60,
+				secret: process.env.NEXT_PUBLIC_SECRET,
+    },
+    callbacks: {
+        redirect: async (url, baseUrl) => {
+            console.log("url", url, "baseUrl", baseUrl);
+            return url.startsWith(baseUrl)
+                ? Promise.resolve(url)
+                : Promise.resolve(baseUrl);
+        },
+    },
+    debug: true,
+};
+
 export default function CustomNextAuth(req, res) {
-    const router = useRouter();
-    return NextAuth(req, res, {
-        providers: [
-            Providers.Google({
-                clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
-                clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
-            }),
-            Providers.GitHub({
-                clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
-                clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
-            }),
-        ],
-        pages: {
-            signIn: "/auth/signin",
-        },
-        database: process.env.DATABASE_URL,
-        secret: process.env.NEXT_PUBLIC_SECRET,
-        session: {
-            jwt: true,
-            maxAge: 30 * 24 * 60 * 60,
-        },
-        callbacks: {
-            async signIn() {
-                return router.push("/");
-            },
-        },
-        debug: false,
-    });
+    return NextAuth(req, res, options);
 }
