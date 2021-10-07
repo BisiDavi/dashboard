2d023c4201e5fabed1f44df1a150105f66d77194
BisiDavi <oludavidconnect@gmail.com>
2021-10-07 23:41:12 +0100

    google analytics configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
+import Script from "next/script";
 import { PersistGate } from "redux-persist/integration/react";
-import {
-    Provider as AuthProvider,
-    useSession,
-    getProviders,
-} from "next-auth/client";
+import { Provider as AuthProvider } from "next-auth/client";
 import { useEffect, useState } from "react";
-import Router, { useRouter } from "next/router";
+import Router from "next/router";
 import NProgress from "nprogress";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -43,26 +40,42 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
     }, []);
 
     return (
-        <AuthProvider
-            options={{ clientMaxAge: 0, keepAlive: 0 }}
-            session={pageProps?.session}
-        >
-            <QueryClientProvider client={queryClient}>
-                <Provider store={store}>
-                    <PersistGate persistor={persistor}>
-                        {Component.auth ? (
-                            <Auth>
-                                <Layout>
-                                    <Component {...pageProps} />
-                                </Layout>
-                            </Auth>
-                        ) : (
-                            <Component {...pageProps} />
-                        )}
-                    </PersistGate>
-                </Provider>
-            </QueryClientProvider>
-        </AuthProvider>
+        <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT}`}
+            />
+            <Script strategy="lazyOnload">
+                {`
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+									gtag('js', new Date());
+									gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT}', {
+										page_path: window.location.pathname,
+									});
+							`}
+            </Script>
+            <AuthProvider
+                options={{ clientMaxAge: 0, keepAlive: 0 }}
+                session={pageProps?.session}
+            >
+                <QueryClientProvider client={queryClient}>
+                    <Provider store={store}>
+                        <PersistGate persistor={persistor}>
+                            {Component.auth ? (
+                                <Auth>
+                                    <Layout>
+                                        <Component {...pageProps} />
+                                    </Layout>
+                                </Auth>
+                            ) : (
+                                <Component {...pageProps} />
+                            )}
+                        </PersistGate>
+                    </Provider>
+                </QueryClientProvider>
+            </AuthProvider>
+        </>
     );
 }
 
